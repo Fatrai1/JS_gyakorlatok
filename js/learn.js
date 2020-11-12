fdae89cff0e9c47678d486606a4bdd8afa9f7b8d
Fatrai2 <midgard@citromail.hu>
2020-11-12 15:55:06 +0100

    14-ig

--- js/learn.js
+++ js/learn.js
@@ -1,7 +1,7 @@
 'use strict';
 //1. Irasd ki egy tetszőleges számú.... legkisebb elemét//
 
-const numericArray = [21, 31, 13, 2, 58, 83, 79, 11, 3, 1, 47];
+const numericArray = [21, 31, 13, 2, 23, 58, 83, 79, 11, 3, 1, 47];
 
 
 let smallest = numericArray[0];
@@ -54,73 +54,196 @@ console.log('5. ' + paros)
 
 //6. Irasd ki egy tetszőleges számú.... második legkisebb elemét//
 
-const minimumIndex = numericArray => {
-    return numericArray.indexOf(Math.min(...numericArray));
- };
- const secondMinimum = numericArray => {
-    const copy = numericArray.slice();
-    copy.splice(minimumIndex(copy), 1);
-    return copy[minimumIndex(copy)];
- };
- console.log(secondMinimum(numericArray));
+function bubbleSortGrowing(arr) {
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped)
+    return arr;
+}
+console.log(bubbleSortGrowing(numericArray)[1]);
 
 
 //7. Irasd ki egy tetszőleges számú.... harmadik legnagyobb elemét//
 
-function bubble_Sort(numericArray) {
-    let swapp;
-    let n = numericArray.length-1;
-    
+function bubbleSortDecreasing(arr) {
+    let swapped;
     do {
-        swapp = false;
-        for (let i=0; i < n; i++)
-        {
-            if (numericArray[i] < numericArray[i+1])
-            {
-               let temp = numericArray[i];
-               numericArray[i] = numericArray[i +1];
-               numericArray[i + 1] = temp;
-               swapp = true;
+        swapped = false;
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] < arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
             }
         }
-        n--;
-    } while (swapp);
- return numericArray; 
+    } while (swapped)
+    return arr;
 }
-console.log(bubble_Sort(numericArray));
-console.log(bubble_Sort(numericArray[2]));
+console.log(bubbleSortDecreasing(numericArray)[2]);
 
-//8. Irasd ki egy tetszőleges számú.... tartalmaz e 23-as számot//
-function find23(numericArray, find) {  
-    let find = 23;
-    let found = false;
-    for (let i = 0; i < numericArray.length && found == false; i++) {	 
-      if (numericArray[i] === find) {				
-        found = true;					
-      }
+
+
+//8. Irasd ki egy DbubbleSortDecreasingetszőleges számú.... tartalmaz e 23-as számot//
+function find23(arr, num) {  
+    for (let i = 0; i < arr.length; i++) {	 
+        if (arr[i] === num) {				
+            return true;					
+        }
     }
+    return false;
 }
-console.log(found);
+let result = find23(numericArray, 23);
+console.log('8. ' + result);
+
 
 
 // 9.Írd ki (igen vagy nem), hogy egy tetszőlegese elemszámú, csak egész számokat tartalmazó tömb tartalmazza-e a 23-as számot! 
 // Használj logaritmikus keresést! Nézz utána mi az, milyen feltételei vannak, és hogyan valósítható meg!
 
+function searchNum(arr, num) {
+    bubbleSortGrowing(arr);
+
+    let start = 0;
+    let end = arr.length - 1;
+    while (start <= end) {
+        let middle = Math.floor ((start + end) / 2);
+        if (arr[middle] === num) {
+            return true;
+        }
+        else if (arr[middle] < num) {
+            start = middle + 1;
+        }
+        else {
+            end =middle -1;
+        }
+    }
+    return false;
+}
+let result1 = searchNum(numericArray, 23);
+console.log('9. ' + result1);
+   
+
+//10. Írd ki, hogy egy tetszőlegese elemszámú, bármilyen típusú elemeket tartalmazó tömb (feltételezzük, hogy beágyazott tömböt,
+// objektumot nem tartalmaz) hányszor tartalmazza a 23-as számot!
+
+function howManyTimes(arr, num) {  
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {	 
+        if (arr[i] === num) {				
+            count++;					
+        }
+    }
+    return count;
+}
+let result2 = howManyTimes(numericArray, 23);
+console.log('10. ' + result2);
+
+
+//11. Rendezd a javított buborékos rendezés algoritmus (nézz utána mi az, ha nem ismered) segítségével egy tetszőleges elemszámú, 
+//csak lebegőpontos számokat tartalmazó tömb elemeit növekvő sorrendbe! Írasd ki a rendezett tömböt!
 
 
 
 
 
-// Írd ki, hogy egy tetszőlegese elemszámú, bármilyen típusú elemeket tartalmazó tömb (feltételezzük, hogy beágyazott tömböt, objektumot nem tartalmaz) hányszor tartalmazza a 23-as számot!
 
-// Rendezd a javított buborékos rendezés algoritmus (nézz utána mi az, ha nem ismered) segítségével egy tetszőleges elemszámú, csak lebegőpontos számokat tartalmazó tömb elemeit növekvő sorrendbe! Írasd ki a rendezett tömböt!
 
-// Rendezd a javított buborékos rendezés algoritmus (nézz utána mi az, ha nem ismered) segítségével egy tetszőleges elemszámú, beágyazott tömb, és objektum kivételével bármilyen típusú elemeket tartalmazó tömb elemeit csökkenő sorrendbe! A nem szám típusú elemeket rakd a tömb végére az eredeti sorrendbe. Írasd ki a rendezett tömböt!
+//11. Rendezd a javított buborékos rendezés algoritmus (nézz utána mi az, ha nem ismered) segítségével egy tetszőleges elemszámú, 
+//beágyazott tömb, és objektum kivételével bármilyen típusú elemeket tartalmazó tömb elemeit csökkenő sorrendbe! 
+//A nem szám típusú elemeket rakd a tömb végére az eredeti sorrendbe. Írasd ki a rendezett tömböt!
 
-// Adott egy tetszőleges elemszámú, csak egész számokat tartalmazó tömb. A tömb elemeit rendezzük növekvő sorrendbe. Ezután a felhasználótól kérjünk be egy számot (addig kérjünk be értéket, míg valóban egy véges egész számot ad meg). A számot szúrjuk be a tömbbe úgy, hogy a tömb továbbra is rendezett maradjon, tehát a megfelelő indexű helyre kerüljön be a plusz elem.
+const numericArray2 = [1, 32, 2, 524, 9845, true, 'egy', 345, 'hatvan', 84, 6356];
+
+function filterNum (arr) {
+    const numbers = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'number'){
+            numbers.push(arr[i])
+        }
+    }
+    return numbers;
+}
+function filterNotNum (arr) {
+    const notNumbers = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] !== 'number'){
+            notNumbers.push(arr[i])
+        }
+    }
+    return notNumbers;
+}
+
+function customSort (arr) {
+    const numbers = filterNum(arr);
+    const notNumbers = filterNotNum(arr);
+    const sortedArr = bubbleSortDecreasing(numbers);
+    return sortedArr.concat(notNumbers);
+}
+console.log('11. ' + customSort(numericArray2));
+
+//12. Adott egy tetszőleges elemszámú, csak egész számokat tartalmazó tömb. A tömb elemeit rendezzük növekvő sorrendbe. 
+//Ezután a felhasználótól kérjünk be egy számot (addig kérjünk be értéket, míg valóban egy véges egész számot ad meg). 
+//A számot szúrjuk be a tömbbe úgy, hogy a tömb továbbra is rendezett maradjon, tehát a megfelelő indexű helyre kerüljön be a plusz elem.
 // Írasd ki a rendezett tömböt!
 
-// Adott egy tömb, mely azonos darabú egész számot, és szöveget tartalmaz véletlenszerű sorrendben. Rendezzük úgy a tömböt, hogy minden szám után egy string következzen! Írasd ki a rendezett tömböt!
+function requestNum (arr) {
+    const inputNum = parseInt(prompt('Adj meg egy számot'));
+    arr.push(inputNum);
+    const sortedArray = bubbleSortGrowing(arr);
+    return sortedArray;
+}
+console.log('12. ' + requestNum(numericArray));
+
+
+
+
+
+//13. Adott egy tömb, mely azonos darabú egész számot, és szöveget tartalmaz véletlenszerű sorrendben. 
+//Rendezzük úgy a tömböt, hogy minden szám után egy string következzen! Írasd ki a rendezett tömböt!
+
+const numString = [23, 54, 65, 'kettő', 'egy', 'négy', 32, 'negyvenöt'];
+
+function filterNum2 (arr) {
+    const numbers = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'number'){
+            numbers.push(arr[i])
+        }
+    }
+    return numbers;
+}
+const number13 = filterNum2(numString);
+
+function filterNotNum2 (arr) {
+    const strings = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] !== 'number'){
+            strings.push(arr[i])
+        }
+    }
+    return strings;
+}
+const string13 = filterNotNum2(numString);
+
+function numAndString (arr) {
+    const numString1 = [];
+    for (let i = 0; i < arr.length/2; i++ ) {
+        numString1.push(number13[i]);
+        numString1.push(string13[i]);
+    }
+    return numString1;
+}
+console.log('13. ' + numAndString(numString));
 
 // Adott egy tetszőleges pozitív egész számokat tartalmazó tömb. Válogassuk szét külön egy even (páros), és odd (páratlan) nevezetű tömbbe a páros, és páratlan számokat! Írjuk ki a 2 tömböt!
 
